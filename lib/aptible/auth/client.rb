2d6f024266ffbab22e1a800685be722e872a3100
Frank Macreery <frank@macreery.com>
2014-02-10 20:38:03 -0500

    Accept any token: Aptible::Auth, Fridge, or String

--- lib/aptible/auth/client.rb
+++ lib/aptible/auth/client.rb
@@ -9,15 +9,24 @@ module Aptible
         unless options.is_a?(Hash)
           fail ArgumentError, 'Call Aptible::Auth::Client.new with a Hash'
         end
-        @token = options[:token]
+        self.token = options[:token]
 
         options[:root] ||= config.root_url
         options[:headers] ||= { 'Content-Type' => 'application/json' }
         options[:headers].merge!(
-          'Authorization' => "Bearer #{options[:token].access_token}"
+          'Authorization' => "Bearer #{bearer_token}"
         ) if options[:token]
 
         super(options)
+      end
+
+      def bearer_token
+        # REVIEW: Should we really allow any token type here?
+        case token
+        when Aptible::Auth::Token then token.access_token
+        when Fridge::AccessToken then token.to_s
+        when String then token
+        end
       end
 
       def config
