b91c8e97a517c92219a8ec6d5e8ddbcb662ce445
Frank Macreery <frank@macreery.com>
2014-04-17 13:12:45 -0400

    Add Organization#security_officer helper method

--- lib/aptible/auth/organization.rb
+++ lib/aptible/auth/organization.rb
@@ -3,6 +3,14 @@ module Aptible
   class Auth::Organization < Auth::Resource
     has_many :roles
 
+    def security_officer
+      # REVIEW: Examine underlying data model for a less arbitrary solution
+      security_officers_role = roles.find do |role|
+        role.name == 'Security Officers'
+      end
+      security_officers_role.users.first if security_officers_role
+    end
+
     def accounts
       api = Aptible::Api.new(token: token, headers: headers)
       api.get unless api.loaded
