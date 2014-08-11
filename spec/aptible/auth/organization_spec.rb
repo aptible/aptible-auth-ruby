3ed589ee747eac28eb16b8e753217439d725eb52
Skylar Anderson <Skylar@Anderson.is>
2014-08-11 13:05:36 -0400

    updated can_manage_compliance? tests

--- spec/aptible/auth/organization_spec.rb
+++ spec/aptible/auth/organization_spec.rb
@@ -1,24 +1,6 @@
 require 'spec_helper'
 
 describe Aptible::Auth::Organization do
-  describe '#can_manage_compliance?' do
-    let(:production) { double 'Aptible::Auth::Resource' }
-    let(:development) { double 'Aptible::Auth::Resource' }
-
-    before { production.stub(:type) { 'production' } }
-    before { development.stub(:type) { 'development' } }
-
-    it 'should return true with a production account' do
-      subject.stub(:accounts) { [development] }
-      expect(subject.can_manage_compliance?).to eq false
-    end
-
-    it 'should return false without a production account' do
-      subject.stub(:accounts) { [development, production] }
-      expect(subject.can_manage_compliance?).to eq true
-    end
-  end
-
   describe '#security_officer' do
     let(:role) { double 'Aptible::Auth::Role' }
     let(:user) { double 'Aptible::Auth::User' }
