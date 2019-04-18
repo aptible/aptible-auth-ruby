75aadf14f4dbdaf6d8ca08ce8fb45ccf150769b7
Jon Zeppieri <jon.zeppieri@careporthealth.com>
2019-04-18 17:32:26 -0400

    Fixed CI problems
    The previous versions of Rake and RSpec did not play well
    together. The issue is described at:
    https://stackoverflow.com/a/35893625
    
    This is apparently fixed in later versions of RSpec. Upgrading to
    RSpec 3 would allow us to un-pin the Rake version.
    
    Also, disabled a rubocop cop the exact same way that the original
    code does it at: https://github.com/oauth-xx/oauth2/blob/65749fc9bb76723d3274f30c6fde20cd3f2d7c5a/lib/oauth2/response.rb#L124.

--- aptible-auth.gemspec
+++ aptible-auth.gemspec
@@ -26,7 +26,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency 'bundler', '>= 1.3', '< 3.0'
   spec.add_development_dependency 'aptible-tasks', '>= 0.2.0'
-  spec.add_development_dependency 'rake'
+  spec.add_development_dependency 'rake', '< 11.0'
   spec.add_development_dependency 'rspec', '~> 2.0'
   spec.add_development_dependency 'rspec-its'
   spec.add_development_dependency 'pry'
--- lib/aptible/auth/oauth2_config.rb
+++ lib/aptible/auth/oauth2_config.rb
@@ -1,5 +1,11 @@
 require 'oauth2'
 
-OAuth2::Response.register_parser(:json, ['application/json', 'text/javascript', 'application/hal+json']) do |body|
-  MultiJson.load(body) rescue body
+json_mime_types = [
+  'application/json',
+  'text/javascript',
+  'application/hal+json'
+]
+
+OAuth2::Response.register_parser(:json, json_mime_types) do |body|
+  MultiJson.load(body) rescue body # rubocop:disable RescueModifier
 end
