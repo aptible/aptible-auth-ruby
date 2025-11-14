52ae7bea333d5818367b8bc6f31a21f37fffba21
T Van Doren <210452321+tvdaptible@users.noreply.github.com>
2025-11-13 17:13:13 -0800

    Update ExternalAwsRole to work! And it can generate an OIDC token.

--- lib/aptible/auth/external_aws_role.rb
+++ lib/aptible/auth/external_aws_role.rb
@@ -11,6 +11,20 @@ module Aptible
       field :last_verified_at, type: Time
       field :created_at, type: Time
       field :updated_at, type: Time
+
+      def external_aws_oidc_token!
+        response = HyperResource::Link.new(
+          self,
+          'href' => "#{href}/external_aws_oidc_token"
+        ).post(
+          self.class.normalize_params(
+            aws_account_id: attributes[:aws_account_id],
+            role_arn: attributes[:role_arn],
+            role_type: attributes[:role_type]
+          )
+        )
+        ExternalAwsOidcToken.new(response.body)
+      end
     end
   end
 end
