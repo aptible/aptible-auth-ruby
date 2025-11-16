ab27d5f8222d6e23025ba8a1c6ed77fe3b1d8812
T Van Doren <210452321+tvdaptible@users.noreply.github.com>
2025-11-15 19:34:44 -0800

    more tests

--- spec/aptible/auth/organization_spec.rb
+++ spec/aptible/auth/organization_spec.rb
@@ -5,8 +5,30 @@ describe Aptible::Auth::Organization do
     let(:user) { double 'Aptible::Auth::User' }
 
     it 'should return the security officer' do
-      subject.stub(:security_officer) { user }
+      allow(subject).to receive(:security_officer) { user }
       expect(subject.security_officer).to eq user
+    end
+  end
+
+  describe '#create_external_aws_role!' do
+    let(:params) do
+      {
+        aws_account_id: '123456789012',
+        role_arn: 'arn:aws:iam::123456789012:role/MyRole',
+        role_type: 'deploy'
+      }
+    end
+    let(:link) { double('HyperResource::Link') }
+
+    before do
+      allow(subject).to receive(:href) { 'https://auth.aptible.com/organizations/1' }
+      allow(HyperResource::Link).to receive(:new).and_return(link)
+      allow(link).to receive(:post)
+    end
+
+    it 'should POST to the external_aws_roles endpoint' do
+      expect(link).to receive(:post).with(params)
+      subject.create_external_aws_role!(params)
     end
   end
 end
